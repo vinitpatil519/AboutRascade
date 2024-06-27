101ea0e0eba88e6e5ac5326c8bdb403bc0a5c796
vinitpatil519 <vinitpatil519@gmail.com>
2024-06-27 14:17:20 +0530

    rocket animation

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+var tl = gsap.timeline()
+
+// Page slide-up animation
 gsap.from(".page2 .aboutrascade24, .page2 p,.page2 .about2,.page2 .vitchennai,.page2 .about3,.page2 .ieeeras", {
   y:200,   
   opacity:0,
   delay:1,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
       trigger:".page2 .aboutrascade24",
       scroller:"body",
@@ -17,7 +20,7 @@ gsap.from(".page2 .rascadelogo, .page2 .raslogo, .page2 .vitlogo", {
   y:150,
   opacity:0,
   delay:1,
-  duration:1,
+  duration:1.5,
   scrollTrigger:{
       trigger:".page2 .aboutrascade24",
       scroller:"body",
@@ -27,3 +30,40 @@ gsap.from(".page2 .rascadelogo, .page2 .raslogo, .page2 .vitlogo", {
       scrub:3,         
   }
 })
+
+
+// ROCKET ANIMATION
+
+
+// tl.to(".page2 .rocket", {
+//   x:740,
+//   delay:1,
+//   duration:0.8,
+//   ease: "power2.out"
+// })
+// tl.to(".page2 .rocket",{
+//   rotate: 0,
+//   delay:0.3,
+//   duration:0.8,
+//   ease: "power2.out"
+// })
+// tl.to(".page2 .rocket", {
+//   y:-450,
+//   delay:0.3,
+//   duration:0.8,
+//   ease: "power2.out"
+// })
+// tl.to(".page2 .rocket",{
+//   rotate: 90,
+//   delay:0.3,
+//   duration:0.8,
+//   ease: "power2.out"
+// })
+// tl.to(".page2 .rocket", {
+//   x:860,
+//   delay:0.3,
+//   duration:1,
+//   opacity: 0,
+//   ease: "power2.out"
+// })
+
